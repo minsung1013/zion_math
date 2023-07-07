1ddbc56fa27d0613571712f26ba742b564d283ae
minsung <minsung1013@gmail.com>
2023-07-07 17:50:37 +0900

    file

--- zion_math.py
+++ zion_math.py
@@ -3,3 +3,20 @@ import streamlit as st
 st.title('최상위 수학 6-2')
 st.subheader('page')
 
+corrects = ['9', '6', '1', '120000', '가', '30240', '50', '01:35', '36']
+problems = ['4-1', '4-2','4-3','5-1: 숫자만','5-2','5-3: 숫자만','6-1: 숫자만','6-2: HH:MM','6-3']
+answers = []
+
+for i in range(len(problems)):
+    form = st.form(problems[i])
+    answers.append(form.text_input(problems[i]))
+
+    if answers[i] == corrects[i]:
+        form.write('good job')
+    else:
+        form.write('sorry, try again')
+
+    form.form_submit_button("Submit")
+
+if corrects == answers:
+    st.balloons()
